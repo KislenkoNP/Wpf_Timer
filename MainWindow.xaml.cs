26102450b029b82a831c247b17a6e2c50ef83bb2
KislenkoNP <kis@sibstrin.ru>
2023-09-20 10:32:31 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Wpf_Timer {
+ public partial class MainWindow : Window {
+  public MainWindow () {
+   InitializeComponent ();
+   DispatcherTimer timer = new DispatcherTimer();
+   timer.Interval = TimeSpan.FromSeconds(1);
+   timer.Tick += 
+    (object sender,EventArgs e) => {
+     label.Content = DateTime.Now.ToLongTimeString ();
+    };
+   timer.Start ();
+  }
+ }
+}
